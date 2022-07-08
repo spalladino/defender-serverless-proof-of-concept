834f0f4dd31fa8b6de8198690e818cae5d2ee0ec
Santiago Palladino <spalladino@gmail.com>
2022-07-08 10:27:37 -0300

    Set schema definition

--- defender-serverless/src/provider.js
+++ defender-serverless/src/provider.js
@@ -1,5 +1,15 @@
 class DefenderProvider {
-  constructor(serverless, options) {}
+  constructor(serverless, options) {
+    const tstring = { type: 'string '};
+    serverless.configSchemaHandler.defineProvider('defender', {
+      function: {
+        properties: {
+          name: tstring,
+          path: tstring,
+        }
+      }
+    })
+  }
 }
 
 module.exports = DefenderProvider;
